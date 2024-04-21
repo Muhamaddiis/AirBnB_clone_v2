f46d505e188067e427aee4aefc2b8dbbd9e1fd20
unknown <khalitiman17@gmail.com>
2024-04-21 17:13:48 +0300

    fixing linting

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Importing Flask framework"""
+"""Imports Flask framework"""
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/", strict_slashes=False)
 
+@app.route("/", strict_slashes=False)
 def hello_world():
-    """return hello HBNB"""
     return "Hello HBNB!"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
